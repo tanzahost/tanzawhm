fa3155e532241d3190ef9a6795ec939319d32ba9
tanzahost <almirfrances0@gmail.com>
2023-11-09 04:21:31 -0800

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>TanzaWHM</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    </head>
+    <body>
+
+        <div class="bg-gray-900 px-6 py-10 p-4 justify-center items-center flex m-10">
+            <div class="text-center flex justify-center items-center">
+                <h1 class="text-2xl font-bold text-white">
+                    TanzaWHM
+                </h1>
+            </div>
+        </div>
+
+    </body>
+</html>
